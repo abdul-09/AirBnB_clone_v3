2b28bea087d4acaaab63f5856b9773ccafb8997b
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-10-30 13:06:05 +0200

    fix task 3

---
+++ api/v1/app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''Contains a Flask web application API.'''
+
+from flask import Flask, make_response, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+
+
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def downtear(self):
+    '''Status of your API'''
+    storage.close()
+
+@app.errorhandler(404)
+def error_404(error):
+    ''' handles 404 error and gives json formatted response '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
